947ccfa47f0e1d799b9d865be9a214dc4b233051
zxthienlao <84703587+zxthienlao@users.noreply.github.com>
2021-06-09 14:19:32 +0500

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/List.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class List<T>
+	{
+		public void Add(T value)
+		{
+			Node<T> node = new Node<T>();
+			node.value = value;
+			if (firstElement == null)
+			{
+				firstElement = node;
+			}
+			else
+			{
+				GetLast().next = node;
+			}
+
+
+		}
+		Node<T> firstElement;
+		Node<T> GetLast()
+		{
+			Node<T> last = firstElement;
+
+			while (last.next != null)
+			{
+				last = last.next;
+			}
+			return last;
+		}
+		public void Print()
+		{
+			Node<T> last = GetFirst();
+			while (last != null)
+			{
+				Console.WriteLine(last.value);
+				last = last.next;
+
+			}
+		}
+		public void AddFront(T value) // выводит предыдущий элемент
+		{
+			Node<T> noded = new Node<T>();
+			noded.value = value;
+
+			if (firstElement == null)
+			{
+				firstElement = noded;
+			}
+			else
+			{
+				noded.next = GetFirst();
+				noded.next.prev = noded;
+			}
+		}
+		Node<T> GetFirst()
+
+		{
+			Node<T> last = firstElement;
+
+			while (last.prev != null)
+			{
+				last = last.prev;
+			}
+			return last;
+		}
+	}
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> sad = new List<int>();
+			Random rnd = new Random();
+			int a = rnd.Next(1, 100);
+			int b = rnd.Next(1, 100);
+			int c = rnd.Next(1, 100);
+			int d = rnd.Next(1, 100);
+			Console.WriteLine(a);
+			Console.WriteLine(b);
+			Console.WriteLine(c);
+			Console.WriteLine(d);
+			Console.WriteLine();
+			sad.Add(a);
+			sad.Add(b);
+			sad.Add(c);
+			sad.AddFront(d);
+			sad.Print();
+			Console.ReadKey();
+		}
+	}
+}
